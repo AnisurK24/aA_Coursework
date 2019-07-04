eefdf0d8ae499b5c78d32d692c0aaab6069c3230
anisurrkhan <anisurk24@gmail.com>
2019-07-03 17:49:38 -0700

    Add mancala homework

--- W6D3/Homework/Mancala/lib/board.rb
+++ W6D3/Homework/Mancala/lib/board.rb
@@ -2,6 +2,8 @@ class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @name1 = name1
+    @name2 = name2
     @cups = Array.new(14) { Array.new }
     place_stones
   end
@@ -14,13 +16,39 @@ class Board
   end
 
   def valid_move?(start_pos)
+    raise "Invalid starting cup" if start_pos < 0 || start_pos > 12
+    raise "Starting cup is empty" if @cups[start_pos].empty?
   end
 
   def make_move(start_pos, current_player_name)
+    stones = @cups[start_pos]
+    @cups[start_pos] = []
+
+    cup_idx = start_pos
+    until stones.empty?
+      cup_idx += 1
+      cup_idx = 0 if cup_idx > 13
+      if cup_idx == 6
+        @cups[6] << stones.pop if current_player_name == @name1
+      elsif cup_idx == 13
+        @cups[13] << stones.pop if current_player_name == @name2
+      else
+        @cups[cup_idx] << stones.pop
+      end
+    end
+    render
+    next_turn(cup_idx)
   end
 
   def next_turn(ending_cup_idx)
     # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+    if ending_cup_idx == 6 || ending_cup_idx == 13
+      :prompt
+    elsif @cups[ending_cup_idx].count == 1
+      :switch
+    else
+    ending_cup_idx
+    end
   end
 
   def render
@@ -32,8 +60,21 @@ class Board
   end
 
   def one_side_empty?
+    @cups[7..12].all? { |cup| cup.empty? } || @cups[0..6].all? { |cup| cup.empty? }
   end
 
   def winner
+    player1 = @cups[6].count
+    player2 = @cups[13].count
+
+    if player1 == player2
+      :draw
+    else
+      if player1 > player2
+        return @name1
+      else
+        return @name2
+      end
+    end
   end
 end
