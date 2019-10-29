c381b8b81fb738141186f10aaa91faa7577528f2
anisurrkhan <anisurk24@gmail.com>
2019-10-28 17:27:51 -0700

    Pass first specs

--- W23D1/linked_list_project/lib/linked_list.js
+++ W23D1/linked_list_project/lib/linked_list.js
@@ -32,7 +32,9 @@ class Node {
 // TODO: Implement a Singly Linked List class here
 class LinkedList {
     constructor() {
-
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
     }
 
     // TODO: Implement the addToTail method here
