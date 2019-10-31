cadd824873bf19c66e3ebfa1be2fe56053010b62
anisurrkhan <anisurk24@gmail.com>
2019-10-30 18:48:52 -0700

    tree order

--- W23D3/tree_order_project/lib/tree_order.js
+++ W23D3/tree_order_project/lib/tree_order.js
@@ -1,9 +1,21 @@
 function inOrderArray(root) {
+    if (!root) return [];
 
+    inOrderArray(root.left, arr);
+    arr.push(root.val);
+    inOrderArray(root.right, arr);
+
+    return arr;
 }
 
 function postOrderArray(root) {
+    if (!root) return [];
 
+    postOrderArray(root.left, arr);
+    postOrderArray(root.right, arr);
+    arr.push(root.val);
+
+    return arr;
 }
 
 
