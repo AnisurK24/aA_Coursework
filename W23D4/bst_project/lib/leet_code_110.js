7fd6478cf59916b0f64e5361af38aeb18f1a4669
anisurrkhan <anisurk24@gmail.com>
2019-10-31 13:22:50 -0700

    Leet code 110

--- W23D4/bst_project/lib/leet_code_110.js
+++ W23D4/bst_project/lib/leet_code_110.js
@@ -1,7 +1,16 @@
 // View the full problem and run the test cases at:
 //  https://leetcode.com/problems/balanced-binary-tree/
 
+const getHeight = (root) => {
+    if (!root) return -1;
+    return 1 + Math.max(getHeight(root.left), getHeight(root.right));
+};
 
 function isBalanced(root) {
-
+    // check the height of each branch
+    // left and right subtrees height diff <= 1
+    if (!root) return true;
+    let heightDifference = Math.abs(getHeight(root.left) - getHeight(root.right)) <= 1;
+      
+    return heightDifference && isBalanced(root.left) && isBalanced(root.right);
 }
