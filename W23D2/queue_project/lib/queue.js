8024fb441d59b9b33f12290250ff1460693fc86c
anisurrkhan <anisurk24@gmail.com>
2019-10-30 18:51:28 -0700

    constructor and enqueue

--- W23D2/queue_project/lib/queue.js
+++ W23D2/queue_project/lib/queue.js
@@ -28,7 +28,23 @@ class Node {
 }
 
 class Queue {
+  constructor() {
+    this.front = null;
+    this.back = null;
+    this.length = 0;
+  }
 
+  enqueue(val) {
+    const newNode = new Node(val);
+    if (!this.front) {
+      this.front = newNode;
+      this.back = newNode;
+    } else {
+      this.back.next = newNode;
+      this.back = newNode;
+    }
+    return ++this.length;
+  }
 }
 
 exports.Node = Node;
