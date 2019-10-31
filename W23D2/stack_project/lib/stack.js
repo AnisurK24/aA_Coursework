e09255a674bd64d2f3e33f3c0023fd3132dcd3f8
anisurrkhan <anisurk24@gmail.com>
2019-10-30 18:55:04 -0700

    constructor push

--- W23D2/stack_project/lib/stack.js
+++ W23D2/stack_project/lib/stack.js
@@ -28,7 +28,27 @@ class Node {
 }
 
 class Stack {
+    constructor() {
+        this.top = null;
+        this.bottom = null;
+        this.length = 0;
+    }
 
+    push(val) {
+        let newNode = new Node(val);
+
+        if (!this.top) {
+        this.top = newNode;
+        this.bottom = newNode;
+        } else {
+        newNode.next = this.top;
+        this.top = newNode;
+        // let temp = this.top;
+        // this.top = newNode;
+        // newNode.next = temp;
+        }
+        return ++this.length;
+    }
 }
 
 exports.Node = Node;
