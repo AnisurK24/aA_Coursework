1f78b0a78e77950721442385123b18faffd6ce45
anisurrkhan <anisurk24@gmail.com>
2019-11-02 20:59:02 -0700

    num regions

--- W23D5/graph_project/lib/num_regions.js
+++ W23D5/graph_project/lib/num_regions.js
@@ -1,5 +1,18 @@
 function numRegions(graph) {
+  let visited = new Set();
+  let count = 0;
+  for (let node in graph) {
+    if (depthFirst(node, graph, visited)) count++;
+  }
+}
 
+function depthFirst(node, graph, visited = new Set()) {
+  if (visited.has(node)) {
+    return;
+  }
+  visited.add(node);
+  graph[node].forEach(neighbor => depthFirst(neighbor, graph, visited));
+  return true;
 }
 
 module.exports = {
