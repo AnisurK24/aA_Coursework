9a87c1fd00896b8785ec6250eafe111b5d96e8f2
anisurrkhan <anisurk24@gmail.com>
2019-11-04 12:14:24 -0800

    get children

--- W24D1/heaps_project/lib/max_heap.js
+++ W24D1/heaps_project/lib/max_heap.js
@@ -5,6 +5,12 @@ class MaxHeap {
     getParent(idx) {
         return Math.floor(idx / 2);
     }
+        getLeftChild(idx){
+        return idx * 2
+    }
+    getRightChild(idx) {
+        return idx * 2 + 1
+    }
 }
 
 module.exports = {
