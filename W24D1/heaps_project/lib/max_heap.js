2390e6b8cd6da28bff3bb600969ef34fae468f61
anisurrkhan <anisurk24@gmail.com>
2019-11-04 12:15:09 -0800

    sift up

--- W24D1/heaps_project/lib/max_heap.js
+++ W24D1/heaps_project/lib/max_heap.js
@@ -6,14 +6,22 @@ class MaxHeap {
         return Math.floor(idx / 2);
     }
     getLeftChild(idx){
-        return idx * 2
+        return idx * 2;
     }
     getRightChild(idx) {
-        return idx * 2 + 1
+        return idx * 2 + 1;
     }
     insert(val) {
-        this.array.push(val)
-        this.siftUp(this.array.length - 1)
+        this.array.push(val);
+        this.siftUp(this.array.length - 1);
+    }
+    siftUp(idx){
+        if(idx === 1) return;
+        let parentIdx = this.getParent(idx);
+        if(this.array[idx] > this.array[parentIdx]){
+            [this.array[idx], this.array[parentIdx]] = [this.array[parentIdx], this.array[idx]];
+            this.siftUp(parentIdx);
+        }        
     }
 }
 
