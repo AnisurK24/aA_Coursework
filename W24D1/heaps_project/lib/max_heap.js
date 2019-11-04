9457fe2ad39cec5c475fcb68a57cb4bfd3afcc4a
anisurrkhan <anisurk24@gmail.com>
2019-11-04 12:14:42 -0800

    insert

--- W24D1/heaps_project/lib/max_heap.js
+++ W24D1/heaps_project/lib/max_heap.js
@@ -5,11 +5,15 @@ class MaxHeap {
     getParent(idx) {
         return Math.floor(idx / 2);
     }
-        getLeftChild(idx){
+    getLeftChild(idx){
         return idx * 2
     }
     getRightChild(idx) {
         return idx * 2 + 1
+    }
+    insert(val) {
+        this.array.push(val)
+        this.siftUp(this.array.length - 1)
     }
 }
 
