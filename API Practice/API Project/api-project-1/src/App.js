8d6e8780fac6d59b73e54f83a76773c7276e78b1
anisurrkhan <anisurk24@gmail.com>
2019-12-09 11:42:30 -0800

    api test

--- API Practice/API Project/api-project-1/src/App.js
+++ API Practice/API Project/api-project-1/src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import Contacts from "./components/contacts";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contacts: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://jsonplaceholder.typicode.com/users")
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ contacts: data });
+      })
+      .catch(console.log);
+  }
+
   render() {
     return (
-      <div>hello</div>
+      <Contacts contacts={this.state.contacts} />
     );
   }
 }
